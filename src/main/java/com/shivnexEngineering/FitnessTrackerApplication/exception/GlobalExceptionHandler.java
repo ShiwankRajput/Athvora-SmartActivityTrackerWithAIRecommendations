7e08f7d3247da867778e161d1d0164208c949912
ShiwankRajput <shiwannnnk@gmail.com>
2026-06-15 17:02:45 +0530

    removed some content

--- src/main/java/com/shivnexEngineering/FitnessTrackerApplication/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shivnexEngineering/FitnessTrackerApplication/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
